ad418cf45c164366402a1bd7f0f016d360dc0021
Alan Nogueira <alansn@gmail.com>
2018-04-03 21:08:55 -0300

    comit 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 //mongodb://alansn:alansncemig31@ds233769.mlab.com:33769/todo-mongodb
-mongoose.connect('mongodb://alansn:cemig31@ds233769.mlab.com:33769/todo-mongodb');
+//mongoose.connect('mongodb://alansn:cemig31@ds233769.mlab.com:33769/todo-mongodb');
 module.exports = {mongoose};
