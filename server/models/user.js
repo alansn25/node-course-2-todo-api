3f937e550dc0688271c79133d74b6719f62fc66f
Alan Nogueira <alansn@gmail.com>
2018-04-10 18:31:12 -0300

    Add tests for GET /users/me and POSt /users

--- server/models/user.js
+++ server/models/user.js
@@ -76,12 +76,11 @@ UserSchema.pre('save', function (next){
 
     if(user.isModified('password')){
         bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(user.password, salt, (err, hash) =>{
-                    
-                    user.password = hash;
-                    next();
-                });
+            bcrypt.hash(user.password, salt, (err, hash) =>{                    
+                user.password = hash;
+                next();
             });
+        });
     }else{
         next();
     }
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,52 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'alan@example.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()        
+    }]
+},{
+    id: userTwoId,
+    email: 'bruna@example.com',
+    password: 'userTwoPass'
+}];
+
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+},{
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(()=> {
+        return Todo.insertMany(todos);
+    }).then(() => done()).catch((e)=>{
+        console.log(e);
+        done();
+    });
+};
+
+const populateUsers = (done) => {
+    User.remove({}).then(()=> {
+        var userOne = new User(users[0]).save();// it is not possible to use insertmany because it wont call the middleware
+        var userTwo = new User(users[1]).save();
+
+       return Promise.all([userOne, userTwo]);// it will wait for both userOne and usertwo to be completed
+    }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
