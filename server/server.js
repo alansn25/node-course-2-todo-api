02f1462b97c3d9b62838771806711c53f41d5b81
Alan Nogueira <alansn@gmail.com>
2018-04-22 16:48:32 -0300

    Changed DEL /todo/:id and POST /users routes to async wait

--- server/server.js
+++ server/server.js
@@ -60,23 +60,44 @@ app.get('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    
+    const id = req.params.id;
+
     if(!ObjectID.isValid(id)){
         return res.status(404).send();
     }
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo)=>{
+    try{
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
         if(!todo){
             return res.status(404).send();
         }
         res.status(200).send({todo});
 
-    }).catch((e)=>{
+    }catch(e){
         res.status(400).send();
-    });
+    }
+    
+    //without async wait  
+    // var id = req.params.id;
+    // if(!ObjectID.isValid(id)){
+    //     return res.status(404).send();
+    // }
+    // Todo.findOneAndRemove({
+    //     _id: id,
+    //     _creator: req.user._id
+    // }).then((todo)=>{
+    //     if(!todo){
+    //         return res.status(404).send();
+    //     }
+    //     res.status(200).send({todo});
+
+    // }).catch((e)=>{
+    //     res.status(400).send();
+    // });
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -109,18 +130,28 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 });
 
 
-app.post('/users', (req, res)=>{
-    //console.log(req.body);
-    var body = _.pick(req.body, ['email', 'password']);
-    var user = new User(body);
-    
-    user.save().then(()=>{
-       return user.generateAuthToken();        
-    }).then((token) => {
+app.post('/users', async (req, res)=>{
+    try{
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = new User(body);
+        await user.save();
+        const token = await user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e)=>{
+    }catch(e){
         res.status(400).send(e);
-    });
+    }
+   //without async wait
+    //console.log(req.body);
+    // var body = _.pick(req.body, ['email', 'password']);
+    // var user = new User(body);
+    
+    // user.save().then(()=>{
+    //    return user.generateAuthToken();        
+    // }).then((token) => {
+    //     res.header('x-auth', token).send(user);
+    // }).catch((e)=>{
+    //     res.status(400).send(e);
+    // });
 });
 
 
@@ -138,26 +169,24 @@ app.get('/users/me', authenticate, (req, res) => {// private route
 });
 
 
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    
-     
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);    
-        });
-        res.send(user);
-    }).catch((e) => {
+app.post('/users/login', async (req, res) => {
+    try{
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    }catch(e){
         res.status(400).send(e);
-    });  
+    }    
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {//private route, you have to be authenticated to access this route
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {//private route, you have to be authenticated to access this route
+    try{
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    }catch(e){
         res.status(400).send();
-    });
+    }    
 });
 
 
